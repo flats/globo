91712b145ba1a448062deeedeb4ca4042da53bd8
flats <dave@robador.com>
2016-02-17 14:31:48 -0500

    add torii to ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,6 +12,8 @@ module.exports = function(defaults) {
     }
   });
 
+  app.import('bower_components/torii/dist/torii.amd.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
