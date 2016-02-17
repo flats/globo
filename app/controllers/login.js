f468cafbf7f5971e392746beef5b3d060fe9794c
flats <dave@robador.com>
2016-02-17 18:33:21 -0500

    application route, authenticator, comment session properties

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
+  // session: Ember.inject.service('session'),
 
   actions: {
-    authenticate() {
-      let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
+    authenticate: function(provider){
+      this.get('session').authenticate('authenticator:torii-oauth2', {
+        torii: this.get('torii'),
+        provider: provider
+      }, function(error) {
+        alert('There was an error when trying to sign you in: ' + error);
       });
     }
   }
