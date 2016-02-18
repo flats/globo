65331a7147086b978ea00ab06e94fcbe675cbce7
flats <dave@robador.com>
2016-02-18 11:39:09 -0500

    pin model

---
+++ app/models/pins.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  nickname: DS.attr('string'),
+  startDate: DS.attr('date'),
+  description: DS.attr('string'),
+  user: DS.belongsTo('user'),
+  place: DS.belongsTo('place'),
+  createdAt: DS.attr('date')
+});
