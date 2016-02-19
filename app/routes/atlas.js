173dc42bd2db6b0eec8388c6b687188f90ed4780
flats <dave@robador.com>
2016-02-19 15:34:55 -0500

    createPin action, save to api

--- app/routes/atlas.js
+++ app/routes/atlas.js
@@ -2,14 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.RSVP.hash({
+    return {
       trips: this.store.findAll('trip'),
       pins: this.store.findAll('pin'),
-      places:this.store.findAll('place'),
+      places: this.store.findAll('place'),
         // adding new, single pin
-      newPin: this.store.createRecord('pin'),
+      // newPin: this.store.createRecord('pin'),
        // adding new trip with multiple pins
       newTrip: this.store.createRecord('trip')
-    });
+    };
+  },
+  actions: {
+    createPin(e) {
+      this.set('newPin', this.store.createRecord('pin', {
+        lat: e.latlng.lat,
+        long: e.latlng.lng
+      }));
+      this.get('newPin').save();
+    }
   }
 });
