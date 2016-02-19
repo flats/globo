7ba07707ed13cd203011972b55263bab40fbf53d
flats <dave@robador.com>
2016-02-18 19:58:13 -0500

    default to zoom 1 lat/long 0

--- app/controllers/pins.js
+++ app/controllers/pins.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   hotel: [45.530891, -122.655504],
-  coordsFor: Ember.computed.map('model.@each.place', function(pin, index) {
-    return [pin.get('place.lat'), pin.get('place.long')];
-  })
+  lat: 0,
+  lng: 0,
+  zoom: 2
 });
