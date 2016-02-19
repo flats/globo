ffdb5efa286bc04ca76b45d1ee59a8eb1b323bc0
flats <dave@robador.com>
2016-02-18 19:41:39 -0500

    delete test property

--- app/controllers/pins.js
+++ app/controllers/pins.js
@@ -4,6 +4,5 @@ export default Ember.Controller.extend({
   hotel: [45.530891, -122.655504],
   coordsFor: Ember.computed.map('model.@each.place', function(pin, index) {
     return [pin.get('place.lat'), pin.get('place.long')];
-  }),
-  names: this.modelFor(this.routeName).get('name');
+  })
 });
