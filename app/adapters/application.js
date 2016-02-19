06ba8b82d7a9460d75b84719b414f48e1d571c62
skipsuva <skip.suva@gmail.com>
2016-02-19 15:44:08 -0500

    add application route mixin

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,8 @@
 import ActiveModelAdapter from 'active-model-adapter';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default ActiveModelAdapter.extend({
-  host: "http://localhost:3000"
+
+export default ActiveModelAdapter.extend(DataAdapterMixin,{
+  host: "http://localhost:3000",
+  authorizer: "authorizer:devise"
 });
