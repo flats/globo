6775261807efa4cb5e0d80a87db2f1eec93d4b0e
flats <dave@robador.com>
2016-02-19 22:07:33 -0500

    delete not destroy, addState default false

--- app/controllers/atlas.js
+++ app/controllers/atlas.js
@@ -5,24 +5,27 @@ export default Ember.Controller.extend({
   lat: 0,
   lng: 0,
   zoom: 2,
-  addState: true,
+  addState: false,
   actions:{
     addDestination(){
       let trip = this.get('model.newTrip');
       let destination = this.store.createRecord('destination');
       trip.get('destinations').addObject(destination);
     },
+    addPinMode() {
+      this.set('addState', true);
+      // $('.leaflet-map-pane').doubleClickZoom.disable();
+    },
     addPoint(e) {
       if(this.get('addState')) {
         this.send('createPin', e);
-        return true;
-      } else {
         // e.target.doubleClickZoom.enable();
-        // this.set('addState', true);
+        this.set('addState', false);
+        return true;
       }
     },
-    delete(pin) {
-      pin.destroyRecord();
+    deletePin(pin) {
+      pin.deleteRecord();
       pin.save();
     }
   }
