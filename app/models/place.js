e3956f18e6c0bf08c2faa2afeef828dadef84add
flats <dave@robador.com>
2016-02-21 18:32:22 -0500

    add ember-data-actions mixins to adapater & model

--- app/models/place.js
+++ app/models/place.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
+import CollectionActionsMixin from 'ember-data-actions/mixins/collection';
 
-export default DS.Model.extend({
+var PlaceModel = DS.Model.extend({
   name: DS.attr('string'),
   lat: DS.attr('number'),
   long: DS.attr('number'),
   createdAt: DS.attr('date'),
   pins: DS.hasMany('pin')
 });
+
+PlaceModel.reopenClass(CollectionActionsMixin);
+
+export default PlaceModel;
