0dab714c8fb8a47b02de6fdec1af3b61238aa671
skipsuva <skip.suva@gmail.com>
2016-02-21 14:34:04 -0500

    try to transition to atlas

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,9 +4,10 @@ export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   actions: {
     authenticate() {
+      let _this = this;
       const { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:devise', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
+      this.get('session').authenticate('authenticator:devise', identification, password).then(()=>{
+        _this.transitionTo('/atlas');
       });
     }
   }
