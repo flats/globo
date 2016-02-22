96ab5ff7712a6cc0c57f7aed7a3e9da0beef7f2d
flats <dave@robador.com>
2016-02-21 22:15:49 -0500

    remove ember data adapter call from app adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,5 @@
 import ActiveModelAdapter from 'active-model-adapter';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-import AdapterActionsMixin from 'ember-data-actions/mixins/adapter';
 
 
 export default ActiveModelAdapter.extend(DataAdapterMixin,{
