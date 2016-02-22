247491a50684bda10457c83b2b8d0b01e5ab773b
flats <dave@robador.com>
2016-02-22 15:55:36 -0500

    fix delete edit save handlers

--- app/components/marker-wrapper.js
+++ app/components/marker-wrapper.js
@@ -4,6 +4,13 @@ export default Ember.Component.extend({
   actions: {
     pinClick() {
       this.sendAction('pinSmack');
+    },
+    savePin(pin){
+      this.sendAction('savePin', pin);
+      this.toggleProperty('editState');
+    },
+    deletePin(pin){
+      this.sendAction('deletePin', pin);
     }
   }
 });
