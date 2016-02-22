e4e3b5e1f15fa247b01486abc7af72bbd3202b6a
skipsuva <skip.suva@gmail.com>
2016-02-22 11:03:50 -0500

    clean up files

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,7 +4,6 @@ export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   actions: {
     authenticate() {
-      let _this = this;
       const { identification, password } = this.getProperties('identification', 'password');
       this.get('session').authenticate('authenticator:devise', identification, password);
     }
