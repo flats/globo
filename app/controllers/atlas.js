a44bd7c18aa119ba8d9838dc53a6b4c9c5fbc4f2
TimoSci <me@timosci.net>
2016-02-23 13:30:18 -0500

    add pin state trips to false when add trip is clicked

--- app/controllers/atlas.js
+++ app/controllers/atlas.js
@@ -27,7 +27,7 @@ export default Ember.Controller.extend({
 
     addPinMode() {
       this.toggleProperty('addState');
-      let addTripState = this.getProperties('addTripState').addTripState
+      let addTripState = this.getProperties('addTripState').addTripState;
       if (addTripState == true) {
         this.toggleProperty('addTripState');
       }
@@ -37,6 +37,10 @@ export default Ember.Controller.extend({
 
     addTripMode(){
       this.toggleProperty('addTripState');
+      let addPinState = this.getProperties('addState').addState;
+      if (addPinState == true) {
+        this.toggleProperty('addState');
+      }
     },
 
     //add click listener when tripMode is true
