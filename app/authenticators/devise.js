84284753dd34534f7307b87b79311a74f1d2929e
flats <dave@robador.com>
2016-02-23 19:47:05 -0500

    https for api

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -6,7 +6,7 @@ export default DeviseAuthenticator.extend({
       if (ENV.environment === 'development') {
         return "http://localhost:3000/users/sign_in";
       } else {
-        return "http://globo-api.herokuapp.com/users/sign_in";
+        return "https://globo-api.herokuapp.com/users/sign_in";
       }
     })()
 });
