f31cc90b362c4014b59cfb45a19c1bab227d95bd
skipsuva <skip.suva@gmail.com>
2016-03-07 12:38:54 -0500

    fix register controller

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -3,14 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     register(){
-      // debugger;
       var newUser = this.store.createRecord('user', {
         name: this.get('name'),
         email: this.get('email'),
         password: this.get('password')
       });
-      // newUser.save();
-      debugger;
+      newUser.save();
+      //add 'then' function if success...not working?
       this.transitionToRoute('login');
     }
   }
